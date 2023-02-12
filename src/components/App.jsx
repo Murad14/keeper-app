53bc4d26db90b5eaa27d708b17be876131e28396
Murad <49407545+Murad14@users.noreply.github.com>
2023-02-13 04:10:12 +0600

    modified

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createNote(noteItem){
+    return <Note
+        title= {noteItem.title}
+        content= {noteItem.content}
+    />
+}
+
 
 function App (){
     return ( 
     <div>
         <Header />
-        <Note/>
+        {notes.map(createNote)}
         <Footer />
     </div>
 );
