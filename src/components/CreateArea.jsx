434ed7eba1796cc516c89a482ca976e84d249a47
Murad <49407545+Murad14@users.noreply.github.com>
2023-02-19 22:23:44 +0600

    add feature done

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
 
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
 
-  function handleChange(event){
+  function handleChange(event) {
+    const { name, value } = event.target;
 
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      }
+    })
+  }
+
+  function submitNote(event) {
+    props.onAdd(note)
+    event.preventDefault();
   }
 
   return (
@@ -27,7 +39,7 @@ function CreateArea() {
           rows="3"
 
         />
-        <button>Add</button>
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
