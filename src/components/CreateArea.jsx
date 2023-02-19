9f8f419c4322d17a5c14697c048946f9a387cb4e
Murad <49407545+Murad14@users.noreply.github.com>
2023-02-19 20:55:47 +0600

    modified

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event){
+
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input
+          onChange={handleChange}
+          name="title"
+          value={note.title}
+          placeholder="Title" />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+
+        />
         <button>Add</button>
       </form>
     </div>
