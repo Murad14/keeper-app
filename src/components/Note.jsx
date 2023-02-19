be69a6d5e357bbc6834f758948eb3b7459d1d0e6
Murad <49407545+Murad14@users.noreply.github.com>
2023-02-20 02:55:07 +0600

    modified

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import DeleteIcon from '@mui/icons-material/Delete';
 
 
 function Note (props){
@@ -12,8 +11,7 @@ function Note (props){
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
-            <button onClick={handleClick}>
-            <DeleteIcon /></button>
+            <button onClick={handleClick}>DELETE</button>
         </div>
     )
 }
