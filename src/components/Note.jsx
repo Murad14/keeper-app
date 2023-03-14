ad449dd7d3a89a11246fc33708d27f5ee70b03bb
Murad <49407545+Murad14@users.noreply.github.com>
2023-03-14 20:51:54 +0600

    modified

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -10,7 +10,7 @@ function Note (props){
     return(
         <div className="note">
             <h1>{props.title}</h1>
-            <p>{props.content}</p>
+            <p>{props.content}</p> //
             <button onClick={handleClick}>DELETE</button>
         </div>
     )
